bf3e6121f093c5d15314d8e6d744aba372f1f4fc
Дарья <makarovada.12345@gmail.com>
2025-10-25 01:36:43 +0300

    первая модель готова

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,6 @@ def set_seed(seed=42):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    torch.cuda.manual_seed_all(seed)  # Если есть GPU
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-
-set_seed(42)
